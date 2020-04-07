3208d6e8b59955708a56a601af777f74950e2ed5
Brent Krone-Schmidt <bawa@deviat.net>
2020-04-07 17:25:39 +0700

    make web.whatsapp happy with useragent

--- renderer.js
+++ renderer.js
@@ -33,9 +33,14 @@ if (tabData) {
 			active: (tabDatum.src == activeTab),
 			iconURL: tabDatum.customFavIconURL || tabDatum.autoFavIconURL,
 			src: tabDatum.src,
-			visible: true
+			visible: true,
+			webviewAttributes: {
+				useragent:
+					"Mozilla/5.0 (X11; Fedora; Linux x86_64) " +
+					"AppleWebKit/537.36 (KHTML, like Gecko) " +
+					"Chrome/80.0.3987.163 Safari/537.36"
+			}
 		});
-
 
 		if (!tabDatum.customFavIconURL) {
 			tab.webview.addEventListener(
