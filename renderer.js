bea9660b980e12e6011f5d2ed4f6be30101af438
Brent Krone-Schmidt <bawa@deviat.net>
2020-12-03 13:01:54 +0700

    fix userAgent

--- renderer.js
+++ renderer.js
@@ -115,7 +115,7 @@ if (tabData) {
 			visible: true,
 			webviewAttributes: {
 				partition: (tabDatum.sessionPartition ? 'persist:' + tabDatum.sessionPartition : ''),
-				useragent:
+				userAgent:
 					"Mozilla/5.0 (X11; Fedora; Linux x86_64) " +
 					"AppleWebKit/537.36 (KHTML, like Gecko) " +
 					"Chrome/87.0.4280.66 Safari/537.36"
